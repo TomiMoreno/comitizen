a3e2ff320dee8781e6e2ea0977994d9fffa98345
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2023-02-13 23:11:03 -0300

    Initial Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import Head from "next/head"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
 import { Layout } from "@/components/layout"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 
 export default function IndexPage() {
+  const [taskDescription, setTaskDescription] = useState("")
+  const [taskName, setTaskName] = useState("")
+  const [commitMessage, setCommitMessage] = useState("")
   return (
     <Layout>
       <Head>
@@ -17,34 +23,36 @@ export default function IndexPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Beautifully designed components <br className="hidden sm:inline" />
-            built with Radix UI and Tailwind CSS.
+      <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:py-10">
+        <div className="flex max-w-[980px] flex-col items-center gap-2">
+          <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+            Turn task descriptions into commits
           </h1>
-          <p className="max-w-[700px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
-            Accessible and customizable components that you can copy and paste
-            into your apps. Free. Open Source. And Next.js 13 Ready.
-          </p>
         </div>
+        <Input
+          className="w-full"
+          placeholder="Enter task name here"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <Textarea
+          className="w-full"
+          value={taskDescription}
+          onChange={(e) => setTaskDescription(e.target.value)}
+          placeholder="Enter task description here"
+        />
         <div className="flex gap-4">
-          <Link
-            href={siteConfig.links.docs}
-            target="_blank"
-            rel="noreferrer"
-            className={buttonVariants({ size: "lg" })}
+          <Button
+            onClick={() => {
+              console.log(taskDescription)
+            }}
+            className={buttonVariants({
+              className: "w-full text-lg",
+              size: "lg",
+            })}
           >
-            Documentation
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            GitHub
-          </Link>
+            Create Commit
+          </Button>
         </div>
       </section>
     </Layout>
